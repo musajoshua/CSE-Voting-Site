09d1796bced35f3d3934056a826f0b0a8d10fe2a
adeojoemmanuel <emmanuel.adojo@yahoo.com>
2018-02-09 22:32:28 +0100

    main setup

--- js/app.js
+++ js/app.js
@@ -1,1 +1,41 @@
-var app;
+var app = angular.module ("cseVote", ['ui.router',]);
+
+app.config(['$urlRouterProvider','$stateProvider','$locationProvider', function($urlRouterProvider,$stateProvider,$locationProvider){
+	$locationProvider.html5Mode({
+	  enabled: true,
+	  requireBase: false
+	});
+	$urlRouterProvider.otherwise('/login');
+
+	$stateProvider
+	.state('login', {
+		url: '/login',
+		templateUrl: './partials/login.html',
+		data: {
+			pageTitle: 'chat'
+		}
+	})
+
+	.state('register', {
+		url: '/register',
+		templateUrl: './partials/register.html',
+		data: {
+			pageTitle: 'register'
+		}
+	})
+
+	.state('categories', {
+		url: '/categories',
+		templateUrl: './partials/categories.html',
+		data: {
+			pageTitle: 'categories'
+		}
+	})
+	.state('contestant', {
+		url: '/contestant',
+		templateUrl: './contestant.html',
+		data: {
+			pageTitle: 'contestant'
+		}
+	})
+}]);

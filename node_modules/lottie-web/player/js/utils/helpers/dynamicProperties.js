cb5443a2ebd822d56dd6aa0a8f2b271b8128a2a9
Musa Joshua <gidijosh@gmail.com>
2020-08-27 22:40:47 +0100

    clean up

--- node_modules/lottie-web/player/js/utils/helpers/dynamicProperties.js
+++
@@ -1,6 +0,0 @@
-function addDynamicProperty(prop) {
-	if(this.dynamicProperties.indexOf(prop) === -1) {
-        this.dynamicProperties.push(prop);
-        this.container.addDynamicProperty(this);
-    }
-}
